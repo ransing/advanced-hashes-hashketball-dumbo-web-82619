eb75e7437095f68a6d8368301d2a680dd8f3be80
virajransing <virajransing@gmail.com>
2019-08-24 13:50:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -105,7 +105,7 @@ def game_hash
 
 
 def num_points_scored(name)
-	game_hash.each do |location, team|
+	game_hash.each do |place, team|
 		team.each do |attribute, data|
  			if attribute == :players
 				data.each do |player|
