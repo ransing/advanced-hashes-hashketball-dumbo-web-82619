dd031c64cf8f276f24323102d50eb9fc5d85bca5
virajransing <virajransing@gmail.com>
2019-08-24 14:00:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -217,7 +217,7 @@ def winning_team
   win_team = ''
   game_hash.each do |away, keys|
     team_points = 0
-    team_name = game_hash[home_away][:team_name]
+    team_name = game_hash[away][:team_name]
     keys[:players].each do |player|
       points = player[:points]
       team_points += points
